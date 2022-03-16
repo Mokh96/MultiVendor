5a4f684564679a41ef98d4f4b34890a81be7ea1f
Mokh96 <58893157+Mokh96@users.noreply.github.com>
2022-03-16 08:24:02 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+
+require("express-async-errors")
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+
+require("./startup/routes")(app);
+require("./routes/prod")(app);
+
+
+app.use((req, res) => {
+    res.status(404).send("404 not Found.")
+})
+
+const port = process.env.PORT || 5000
+app.listen(port ,()=> console.log(`Listening on Port ${port} ...`))
